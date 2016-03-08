d84fe820fb37a7cac003d6638b747da5f31c3c3f
Žiga S <zigsim1@gmail.com>
2016-03-08 18:09:02 +0000

    Stran pripravljena za produkcijo

--- opomniki/skripta.js
+++ opomniki/skripta.js
@@ -7,11 +7,18 @@ window.addEventListener('load', function() {
 		for (i = 0; i < opomniki.length; i++) {
 			var opomnik = opomniki[i];
 			var casovnik = opomnik.querySelector("span");
+			var naziv = opomnik.querySelector(".naziv_opomnika").innerHTML;
 			var cas = parseInt(casovnik.innerHTML);
 	
 			//TODO: 
 			// - če je čas enak 0, izpiši opozorilo "Opomnik!\n\nZadolžitev NAZIV_OPOMNIK je potekla!"
 			// - sicer zmanjšaj čas za 1 in nastavi novo vrednost v časovniku
+			if (cas <= 0) {
+				alert("Opomnik!\nZadolžitev "+naziv+" je potekla!");
+			} else {
+				cas--;
+				casovnik.innerHTML = cas;
+			}
 		}
 	}
 	setInterval(posodobiOpomnike, 1000);
@@ -32,7 +39,7 @@ window.addEventListener('load', function() {
 		document.querySelector("#naziv_opomnika").value = "";
 		document.querySelector("#cas_opomnika").value = "";
 		
-		document.querySelector("#opomniki").innerHTML += "<div class='opomnik'> \
+		document.querySelector("#opomniki").innerHTML += "<div class='opomnik rob senca'> \
             <div class='naziv_opomnika'>"+naziv+"</div> \
             <div class='cas_opomnika'> Opomnik čez <span>"+cas+"</span> sekund.</div> \
 			</div>";
